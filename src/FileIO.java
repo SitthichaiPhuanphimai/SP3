ccfe6517e31107dd2afa2af075f23553b7183b65
meti100 <metinakbas2010@gmail.com>
2022-11-03 14:16:46 +0100

    FileIO og data mappe

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,19 +1,18 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class FileIO
 {
 
     public ArrayList<String> readMovieData()
     {
-        File file = new File("Data/movie.txt");
+        File file = new File("Data/movie.csv");
         ArrayList<String> data = new ArrayList<>();
 
         try
         {
             BufferedReader in = new BufferedReader(
-                    new FileReader("data/movie.txt"));
+                    new FileReader("data/movie.csv"));
             String str;
 
             while ((str = in.readLine()) != null)
