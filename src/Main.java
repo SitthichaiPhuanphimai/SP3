c1f27aeaa5502c1e7637ee23780e29069574d1c5
VivekNagra <71701790+VivekNagra@users.noreply.github.com>
2022-11-04 13:58:06 +0100

    friday changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-public class Main {
+import java.util.ArrayList;
+
+public class Main
+{
 
     public static void main(String[] args) {
 
-        RunProgram.run();
+
+
+//        ArrayList<String> data =  FileIO.();
+//        if(data == null){
+//            data = textUI.getUserInput("Skriv spillernavn. Tast Q for at quitte",6);
+//        }
+//        RunProgram.run();
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,15 +1,25 @@
 public class Movie extends Media
 {
     private String name;
+    private String genre;
     private int rating;
     private boolean hasWatched;
+    private int releaseYear;
 
-    public Movie(String name, int rating, boolean hasWatched)
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public Movie(String name, int releaseYear, String genre, int rating )
     {
         this.name = name;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
         this.rating = rating;
-        this.hasWatched = hasWatched;
+        //this.hasWatched = hasWatched;
     }
+
 
 
     public String getName()
@@ -26,4 +36,10 @@ public class Movie extends Media
     {
         return hasWatched;
     }
+
+    public int getReleaseYear()
+    {
+        return releaseYear;
+    }
+
 }
